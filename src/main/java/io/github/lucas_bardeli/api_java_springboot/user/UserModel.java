dc39ed2f6359207649479b07248bde4d97f3d6ba
lucas-bardeli <lucasbbsjau@gmail.com>
2026-08-16 13:04:45 -0300

    Facilitando os getters e setters com Lombok

--- src/main/java/io/github/lucas_bardeli/api_java_springboot/user/UserModel.java
+++ src/main/java/io/github/lucas_bardeli/api_java_springboot/user/UserModel.java
@@ -1,34 +1,11 @@
 package io.github.lucas_bardeli.api_java_springboot.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
   private String username;
   private String name;
   private String password;
-
-  // Getters
-  public String getUsername() {
-    return username;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  // Setters
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
 }
